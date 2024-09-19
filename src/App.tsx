abba257a8ba2c87381da2e2bc29911e72dfe7c3c
fariaotavio12 <otaviofaria30@gmail.com>
2024-09-19 14:17:44 -0300

    Ajuste Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-
 import Calculadora from "./components/module/calculadora";
-import { ThemeProvider } from "./lib/context/theme";
 
 function App() {
 	return (
 		<>
-			<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-				<Calculadora />
-			</ThemeProvider>
+			<Calculadora />
 		</>
 	);
 }
